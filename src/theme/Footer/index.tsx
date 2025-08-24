ccfa8c71baac8f2292d759a30cf18e38008dcfda
TrueBankai416 <97103466+TrueBankai416@users.noreply.github.com>
2025-08-24 01:23:49 -0400

    Update index.tsx
    Enable floating widgets

--- src/theme/Footer/index.tsx
+++ src/theme/Footer/index.tsx
@@ -1,20 +1,17 @@
 import React from 'react'
 import Footer from '@theme-original/Footer'
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
-import { MendableFloatingButton } from '@mendable/search'
 import BuyMeACoffeeFloatingWidget from '@site/src/components/BuyMeACoffeeFloatingWidget'
+import DiscordFloatingWidget from '@site/src/components/DiscordFloatingWidget'
 
 export default function FooterWrapper(props) {
   const {
     siteConfig: { customFields },
   } = useDocusaurusContext()
-  
-  // Fixed TypeScript error - explicit type casting for mendableAnonKey
-  const mendableKey = customFields.mendableAnonKey as string;
 
   return (
     <>
-      <MendableFloatingButton anon_key={mendableKey} />
+      <DiscordFloatingWidget />
       <BuyMeACoffeeFloatingWidget />
       <Footer {...props} />
     </>
