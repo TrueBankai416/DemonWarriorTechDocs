888994b93b9af038a5ad9003085fbe60e14b760f
TrueBankai416 <97103466+TrueBankai416@users.noreply.github.com>
2025-08-24 01:55:42 -0400

    Update docusaurus.config.js
    Add GitHub change history

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -47,6 +47,8 @@ const config = {
         docs: {
           routeBasePath: '/', // Serve the docs at the site's root
           sidebarPath: './sidebars.js',
+          showLastUpdateAuthor: true,
+          showLastUpdateTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
 //          editUrl:
