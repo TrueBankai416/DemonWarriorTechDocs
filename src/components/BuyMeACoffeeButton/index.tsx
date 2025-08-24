1428efd32edf2bfb8abccc71b6fa4f1c4ceeb124
TrueBankai416 <97103466+TrueBankai416@users.noreply.github.com>
2025-08-24 02:13:40 -0400

    Update index.tsx

--- src/components/BuyMeACoffeeButton/index.tsx
+++ src/components/BuyMeACoffeeButton/index.tsx
@@ -7,7 +7,7 @@ interface BuyMeACoffeeButtonProps {
 const BuyMeACoffeeButton: React.FC<BuyMeACoffeeButtonProps> = ({ className }) => {
   return (
     <a href="https://buymeacoffee.com/demonwarriortech" className={className}>
-      <img src="/img/buymeacoffee-button.svg" alt="Buy me a beer" />
+      <img src="/img/buymeacoffee-button.svg" alt="Buy me pc parts" />
     </a>
   );
 };
