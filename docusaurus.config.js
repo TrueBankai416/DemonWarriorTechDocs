fd57823106d57aa5bcc9204cbbafae1ec10c63e3
DevilsCoder <35183970+DevilsDesigns@users.noreply.github.com>
2025-08-23 20:39:17 -0500

    Add files via upload

---
+++ docusaurus.config.js
@@ -0,0 +1,156 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import {themes as prismThemes} from 'prism-react-renderer';
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Demon Warrior Tech Docs',
+  tagline: 'Docs',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://docs.demonwarriortech.com',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          routeBasePath: '/', // Serve the docs at the site's root
+          sidebarPath: './sidebars.js',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+//          editUrl:
+//            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+ //       blog: {
+//         showReadingTime: true,
+//          // Please change this to your repo.
+//          // Remove this to remove the "edit this page" links.
+//          editUrl:
+//            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+//        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+  ],
+
+  themeConfig: 
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    
+    ({
+      announcementBar: {
+        id: 'Pick your Operating System',
+        content:
+          'This site is mainly for Windows Systems. For Ubuntu/Linux, visit <a target="_blank" rel="noopener noreferrer" href="https://docs.bankai-tech.com">Bankai Tech Docs</a>',
+        backgroundColor: '#fafbfc',
+        textColor: '#091E42',
+        isCloseable: true,
+      },
+      colorMode: {
+        defaultMode: 'dark',
+        disableSwitch: false,
+        respectPrefersColorScheme: false,
+      },
+      // Replace with your project's social card
+      image: 'img/docusaurus.png',
+      navbar: {
+        title: 'Demon Warrior Tech Docs',
+        logo: {
+          alt: 'DWT logo',
+          src: 'img/docusaurus.png',
+        },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'tutorialSidebar',
+            position: 'left',
+            label: 'Tutorial',
+            to: '/',
+          },
+          {to: 'https://docs.demonwarriortech.com/Video%20Tutorials/Videos', label: 'Videos Tutorials', position: 'left'},
+          {to: 'https://buymeacoffee.com/demonwarriortech', label: 'Buy Me a Coffee', position: 'left'},
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Documented-Tutorial',
+                to: '/category/Documented-Tutorials-',
+              },
+              {
+                label: 'Examples',
+                to: '/category/Examples',
+              },
+              {
+                label: 'Jellyfin Extras',
+                to: '/category/Jellyfin-Extras',
+              },
+              {
+                label: 'Arr Self-Hosting',
+                to: 'https://docs.demonwarriortech.com/Arr%20Self-Hosting/Arr%20Self-Hosted',
+              },
+              {
+                label: 'Videos',
+                to: 'https://docs.demonwarriortech.com/Video%20Tutorials/Videos',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Youtube',
+                href: 'https://www.youtube.com/@DemonWarriorTech',
+              },
+              {
+                label: 'Discord',
+                href: 'https://discord.com/invite/9DDRsn3jxD',
+              },
+              {
+                label: 'Buy Me A PC Part',
+                href: 'https://www.buymeacoffee.com/demonwarriortech',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Demon Warrior Tech Docs, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+    plugins: [require.resolve('docusaurus-lunr-search')],
+};
+
+export default config;
